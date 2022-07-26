439f0ea9a2fcd784c42d66d8f02adf5ebda9d727
Jon Gorman <jtgorman@illinois.edu>
2022-07-26 14:41:01 -0500

    Initial set of gem files created via bundler

---
+++ lib/uiuc_lib_ad.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "uiuc_lib_ad/version"
+
+module UiucLibAd
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "uiuc_lib_ad"
+
+require "test-unit"
